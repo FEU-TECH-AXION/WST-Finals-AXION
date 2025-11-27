0b044d54ce7a4824be7a26f5711dc697716d7e82
sdoxina <202311026@fit.edu.ph>
2025-11-28 06:31:59 +0800

    fix(Config/App): fix baseurl

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/samples/tw33/';
+    public string $baseURL = 'http://localhost/WST-Finals-AXION';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
