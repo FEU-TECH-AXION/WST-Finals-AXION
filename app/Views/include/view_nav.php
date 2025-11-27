434bfa0456ab5fe88b3c167f5ba7f98b097a1ce5
sdoxina <202311026@fit.edu.ph>
2025-11-28 07:09:17 +0800

    refactor(navbar): update navbar style and add logo placeholder

--- app/Views/include/view_nav.php
+++ app/Views/include/view_nav.php
@@ -1,36 +1,61 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="<?= base_url(); ?>">TW32</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="<?= base_url(); ?>">
+            <img src="<?= base_url('/public/assets/img/logoPlaceholder.png'); ?>" alt="AXION Logo" height="40" class="d-inline-block align-text-top">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Navigation Links -->
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <!-- User Management (ITSO Personnel Only) -->
                 <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="<?= base_url('users');?>">View Users List</a>
+                    <a class="nav-link" href="<?= base_url('users'); ?>">User Management</a>
                 </li>
+
+                <!-- Equipment Management -->
                 <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="<?= base_url('equipment'); ?>">Equipment</a>
                 </li>
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-                </li>
+
+                <!-- Borrowing Module -->
                 <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    <a class="nav-link" href="<?= base_url('borrow'); ?>">Borrowing</a>
+                </li>
+
+                <!-- Return Module -->
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('returns'); ?>">Returns</a>
+                </li>
+
+                <!-- Reservation Module (Associates only) -->
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('reservations'); ?>">Reservations</a>
+                </li>
+
+                <!-- Reports -->
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('reports'); ?>">Reports</a>
+                </li>
+
+                <!-- About Page -->
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('about'); ?>">About</a>
                 </li>
             </ul>
-            <form class="d-flex" role="search">
+
+            <!-- Search Form -->
+            <form class="d-flex me-2" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
                 <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
-            </div>
+
+            <!-- Login Button -->
+            <a href="<?= base_url('login'); ?>" class="btn btn-login">Log In</a>
         </div>
-    </nav>
+    </div>
+</nav>
