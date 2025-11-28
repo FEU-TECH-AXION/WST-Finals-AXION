e5fd90f863a64dc9361b262f0010eb7e02949171
mm-gonzales <michaela.v.gonzales@gmail.com>
2025-11-29 00:11:52 +0800

    feat: add model for borrow log

---
+++ app/Models/Model_Borrow_Log.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Model_Users extends Model{
+    protected $table      = 'borrow_log';
+   
+    protected $primaryKey = 'borrow_id';
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'array'; // object
+    // protected $useSoftDeletes = true;
+
+    protected $allowedFields = [
+        'borrow_id',
+        'item_id', 
+        'user_id', 
+        'borrow_date', 
+        'expected_return_date',
+        'status', 
+        'date_created',
+        'date_updated',
+    ];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected $useTimestamps = true;
+    protected $createdField  = 'date_created';
+    protected $updatedField  = 'date_updated';
+}
+?>
