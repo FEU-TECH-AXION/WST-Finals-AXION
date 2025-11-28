619d2c3d42469a3e417b6fb2e1c48ac82aaa30e6
sdoxina <202311026@fit.edu.ph>
2025-11-28 23:27:43 +0800

    feat(Routes): add routes for quipments controllers

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -19,4 +19,13 @@ $routes->post('users/update/(:num)', 'Users::update/$1');
 $routes->get('users/delete/(:num)', 'Users::delete/$1');
 $routes->get('users/verify/(:any)', 'Users::verify/$1');
 
+// Routes for the Equipments controllers
+$routes->get('equipments', 'Equipments::index');
+$routes->get('equipments/equipment_add', 'Equipments::add');
+$routes->post('equipments/equipment_insert', 'Equipments::insert');
+$routes->get('equipments/equipment/(:num)', 'Equipments::view/$1');
+$routes->get('equipments/equipment_edit/(:num)', 'Equipments::edit/$1');
+$routes->post('equipments/equipment_update/(:num)', 'Equipments::update/$1');
+$routes->get('equipments/equipment_delete/(:num)', 'Equipments::delete/$1');
+
 // Routes for the ____ controllers
