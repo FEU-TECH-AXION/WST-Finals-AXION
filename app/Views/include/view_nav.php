177a89e8aacf95f9b2af624f5d10e8299740692c
sdoxina <202311026@fit.edu.ph>
2025-11-29 19:44:32 +0800

    refactor: edit nav logo color

--- app/Views/include/view_nav.php
+++ app/Views/include/view_nav.php
@@ -1,25 +1,8 @@
-<?php
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="<?= base_url('/public/assets/img/logoPlaceholder.png') ?>">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="<?= base_url('/public/assets/css/style.css') ?>">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
-    <title><?= esc($title ?? 'ITSO'); ?></title>
-</head>
-<body>
     <!-- Navigation Bar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <div class="container">
             <a class="navbar-brand d-flex align-items-center" href="<?= base_url('/') ?>">
-                <img src="<?= base_url('/public/assets/img/logoPlaceholder.png') ?>" alt="Logo" height="40" class="me-2">
-                ITSO
+                <img src="<?= base_url('/public/assets/img/logoPurple.png') ?>" alt="Logo" height="40" class="me-2">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
